a9340a676e430ef5bf5c62e9fca683f196aff51a
Victor Ehebhoria <victorehebhoria@gmail.com>
2025-11-25 16:46:48 +0000

    change this from "mood" to "mood for the day" (_for element <CardTitle>_

--- src/components/dashboard/DashboardMetrics.tsx
+++ src/components/dashboard/DashboardMetrics.tsx
@@ -43,7 +43,7 @@ export default function DashboardMetrics() {
         
         <Card>
             <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-              <CardTitle className="text-sm font-medium">Mood</CardTitle>
+              <CardTitle className="text-sm font-medium">Mood for the day</CardTitle>
               <Smile className={`h-4 w-4 text-muted-foreground ${moodColor}`} />
             </CardHeader>
             <CardContent>
