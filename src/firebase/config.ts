673c040a68e83566b6eb3b91d94953fda061c927
Victor Ehebhoria <victorehebhoria@gmail.com>
2025-11-25 23:46:04 +0000

    please fix the error ## Error Type Console FirebaseError
    ## Error Messa

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:1072461112497:web:037288a752f8b374d9ee90",
   "apiKey": "AIzaSyCXXNShHn547cV_82b-Hbk-sF82Oz4H6_k",
   "authDomain": "studio-8972653883-d4ab8.firebaseapp.com",
+  "storageBucket": "studio-8972653883-d4ab8.appspot.com",
   "measurementId": "",
   "messagingSenderId": "1072461112497"
 };
